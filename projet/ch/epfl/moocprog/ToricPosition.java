9eaff154a5ad70556de8087dab5178b544c9a3fb
Abdou AFIFI <afifiabdou@gmail.com>
2020-12-17 14:56:05 +0100

    ToricPosition.Constructors

--- projet/ch/epfl/moocprog/ToricPosition.java
+++ projet/ch/epfl/moocprog/ToricPosition.java
@@ -11,7 +11,8 @@ public final class ToricPosition {
 	private static Vec2d clampedPosition(double x, double y)
 	{
 		int worldWidth = getConfig().getInt(WORLD_WIDTH) ; 
-		
+		int worldHeight = getConfig().getInt(WORLD_HEIGHT) ; 
+
 		while(x <0 )
 		{
 			x = x + worldWidth ; 
@@ -21,13 +22,39 @@ public final class ToricPosition {
 		{
 			x = x - worldWidth ; 
 			
+		}		
+		
+		
+		while(y <0 )
+		{
+			y = y + worldHeight ; 
+			
 		}
+		while(y> worldWidth  )
+		{
+			y = y - worldHeight ; 
+			
+		}
+		return new Vec2d(x, y);
 	}
 	
 
-	public ToricPosition()
+	public ToricPosition(double newx, double newy)
 	{
+		position = clampedPosition(newx, newy);
 		
 	}
+	public ToricPosition(Vec2d newposition)
+	{
+		position = clampedPosition(newposition.getX() , newposition.getY());
+		
+	}
+	
+	public ToricPosition()
+	{
+		position = clampedPosition(0.0 ,0.0);
+		
+	}
+
 
 }
